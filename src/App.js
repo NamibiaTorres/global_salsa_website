200f96c7904c563814150fe56aa21d579b2a0a0a
NLT <NamibiaLT@users.noreply.github.com>
2019-07-02 21:32:06 -0700

    update src/app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,15 +7,16 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+
+          <h2>Welcome to The Global Salsa Website</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started search for the country in which you are dancing!
         </p>
       </div>
     );
   }
 }
-
+// To add a logo, insert above "Welcome to Global Salsa Website":
+// <img src={logo} className="App-logo" alt="logo" />
 export default App;
